7b31fe33f3aaf9bdc539585fb43dba4f1c11dadf
Usama Sadiq <=>
2020-11-28 13:22:12 +0500

    Changes in AddTransaction.js file, TransactionList.js and add Transaction.js file
    Add income expense data via form and add data list in history section

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,23 +1,16 @@
+import { useContext } from "react";
+import Transaction from "./Transaction";
+import { GlobalContext } from "../context/GlobalState";
+
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
   return (
     <>
       <h3>History</h3>
       <ul className="list">
-        <li className="plus">
-          Camera
-          <span>+$100</span>
-          <button className="delete-btn">x</button>
-        </li>
-        <li className="minus">
-          Book
-          <span>-$300</span>
-          <button className="delete-btn">x</button>
-        </li>
-        <li className="plus">
-          Pen
-          <span>+$30</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
