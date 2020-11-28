75497b455312370442f769402f4401602d931682
Usama Sadiq <=>
2020-11-28 12:58:13 +0500

    Create GlobalState.js and AppReducer.js file in context folder

---
+++ src/context/GlobalState.js
@@ -0,0 +1,41 @@
+import { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+
+// Initial State
+const initialState = {
+  transactions: [],
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        addTransaction,
+        deleteTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
